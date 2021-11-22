92a98548253fd8dd10b769150beb272191cb34ce
cky <1140051355@qq.com>
2021-11-22 16:37:57 +0800

    更新主界面

--- lib/component/cky-page.vue
+++ lib/component/cky-page.vue
@@ -1,19 +1,80 @@
 <template>
     <div>
-        {{message}}
+        <el-row :[pageConfig.mode]="pageConfig[pageConfig.mode]" :gutter="pageConfig.gutter">
+            <el-col :span="pageItem.width" :key="filId" v-for="(pageItem,filId) in computedPageDesc">
+                <component
+                    :is="`${pageItem.type}`"
+                    v-model="pageConfig.pageData[filId]"
+                >
+                </component>
+            </el-col>
+        </el-row>
     </div>
 </template>
 
 <script setup>
-    import {defineProps} from 'vue'
+    import {defineProps, computed, toRefs} from 'vue'
+    import pageConfigDefault from '../config/pageConfig'
+
     const props = defineProps({
-        message: {
-            type: String,
-            default: '纠结啊'
+        pageConfig: {
+            type: Object,
+            default: () => pageConfigDefault
+        }
+    });
+
+    // 计算排序后的pageDesc
+    const computedPageDesc = computed(() => {
+        let orderPageDesc = {};//排序后的页面描述
+        if (props.pageConfig.order && props.pageConfig.order.length) { //如果有order
+            props.pageConfig.order.forEach((filId) => {
+                if (props.pageConfig.pageDesc[filId]) {
+                    Reflect.set(orderPageDesc, filId, props.pageConfig.pageDesc[filId])
+                }
+                else {
+                    throw new Error('order中定义的key在pageDesc中不存在')
+                }
+            });
+            return orderPageDesc
+        }
+        else { //无order按照原来序列排列
+            orderPageDesc = props.pageConfig.pageDesc;
+            return orderPageDesc
         }
     });
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+    .el-row {
+        margin-bottom: 20px;
+        &:last-child {
+            margin-bottom: 0;
+        }
+    }
 
+    .el-col {
+        border-radius: 4px;
+    }
+
+    .bg-purple-dark {
+        background: #99a9bf;
+    }
+
+    .bg-purple {
+        background: #d3dce6;
+    }
+
+    .bg-purple-light {
+        background: #e5e9f2;
+    }
+
+    .grid-content {
+        border-radius: 4px;
+        min-height: 36px;
+    }
+
+    .row-bg {
+        padding: 10px 0;
+        background-color: #f9fafc;
+    }
 </style>
--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,48 @@
 <template>
-    <div>
-        <img alt="Vue logo" src="./assets/logo.png">
-        <cky-page message="666"></cky-page>
-    </div>
-
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-
+    <!--<img alt="Vue logo" src="./assets/logo.png">-->
+    <cky-page :pageConfig="pageConfig"></cky-page>
+    <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
 </template>
 
 <script>
     import HelloWorld from './components/HelloWorld.vue'
-
     export default {
         name: 'App',
-        data: () => {
-            return {
-                a: `1454153`,
-                message: '56465'
-            }
-        },
         components: {
             HelloWorld
+        },
+        data() {
+            return {
+                pageConfig: {
+                    mode: 'justify',//排列方式
+                    justify: 'center',//默认采用flex对齐方式
+                    align: 'middle',
+                    gutter: 10,//块间距
+                    pageData: {
+                        key: '哈哈哈',
+                        key1: "66",
+                    },
+                    pageDesc:{
+                        key: {
+                            type: 'el-input',
+                            width: 6,
+                        },
+                        key1: {
+                            type: 'el-input',
+                            width: 6,
+                        }
+                    },
+                    order: ['key1','key']
+                }
+            }
+        },
+        watch: {
+            'pageConfig.pageData': {
+                handler(value) {
+                    console.log(value)
+                },
+                deep: true
+            }
         }
     }
 </script>
